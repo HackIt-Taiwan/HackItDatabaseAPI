4c65719843b0ec1724742223911ac414176a71cd
Yorukot <ben.hrc57@gmail.com>
2024-12-01 00:47:10 +0800

    feat: update staff presmossion level and current group

--- app/models/staff_model.go
+++ app/models/staff_model.go
@@ -18,8 +18,8 @@ type Staff struct {
 	EmergencyContact []EmergencyContact `json:"emergency_contact,omitempty" bson:"emergency_contact,omitempty" binding:"min=1,max=2,dive" encryption:"true"`
 	DiscordID        string             `json:"discord_id,omitempty" bson:"discord_id,omitempty" binding:"max=30"`
 	Introduction     string             `json:"introduction,omitempty" bson:"introduction,omitempty" binding:"max=1000" encryption:"true"`
-	CurrentGroup     string             `json:"current_group" bson:"current_group" binding:"required,oneof=HackIt 策劃部 設計部 行政部 公關組 活動企劃組 美術組 資訊組 影音組 行政組 財務組 其他"`
-	PermissionLevel  int                `json:"permission_level" bson:"permission_level" binding:"required,oneof=1 2 3 4 5 6"`
+	CurrentGroup     string             `json:"current_group" bson:"current_group" binding:"required,oneof=HackIt 行政部 公共事務部 策劃部 媒體影像部 資訊科技部"`
+	PermissionLevel  int                `json:"permission_level" bson:"permission_level" binding:"required,oneof=1 2 3 4 5 6 0"`
 	CreatedAt        time.Time          `json:"created_at" bson:"created_at"`
 }
 
