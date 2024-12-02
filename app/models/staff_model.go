8d58b75b46a64afdf7024cc159d80d67b00bf104
Yorukot <ben.hrc57@gmail.com>
2024-12-02 12:14:28 +0800

    feat: update team leader

--- app/models/staff_model.go
+++ app/models/staff_model.go
@@ -20,6 +20,7 @@ type Staff struct {
 	Introduction     string             `json:"introduction,omitempty" bson:"introduction,omitempty" binding:"max=1000" encryption:"true"`
 	CurrentGroup     string             `json:"current_group" bson:"current_group" binding:"required,oneof=HackIt 行政部 公共事務部 策劃部 媒體影像部 資訊科技部"`
 	PermissionLevel  int                `json:"permission_level" bson:"permission_level" binding:"required,oneof=1 2 3 4 5 6 0 10"`
+	TeamLeader       string             `json:"team_leader,omitempty" bson:"team_leader,omitempty" binding:"max=30"`
 	CreatedAt        time.Time          `json:"created_at" bson:"created_at"`
 }
 
@@ -43,8 +44,9 @@ type UpdateStaff struct {
 	EmergencyContact []EmergencyContact `json:"emergency_contact,omitempty" bson:"emergency_contact,omitempty" binding:"max=2,dive" encryption:"true"`
 	DiscordID        string             `json:"discord_id,omitempty" bson:"discord_id,omitempty" binding:"max=30"`
 	Introduction     string             `json:"introduction,omitempty" bson:"introduction,omitempty" binding:"max=1000" encryption:"true"`
-	CurrentGroup     string             `json:"current_group,omitempty" bson:"current_group,omitempty" binding:"oneof=HackIt 策劃部 設計部 行政部 公關組 活動企劃組 美術組 資訊組 影音組 行政組 財務組 其他"`
-	PermissionLevel  int                `json:"permission_level,omitempty" bson:"permission_level,omitempty" binding:"oneof=1 2 3 4 5 6"`
+	CurrentGroup     string             `json:"current_group,omitempty" bson:"current_group,omitempty" binding:"oneof=HackIt 行政部 公共事務部 策劃部 媒體影像部 資訊科技部"`
+	TeamLeader       string             `json:"team_leader,omitempty" bson:"team_leader,omitempty" binding:"max=30"`
+	PermissionLevel  int                `json:"permission_level,omitempty" bson:"permission_level,omitempty" binding:"oneof=1 2 3 4 5 6 0 10"`
 }
 
 type GetStaff struct {
